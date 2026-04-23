8b4f728d475e1bb72a8c7573b07ee24636c8b27c
Arek <arek@arek.com>
2026-04-23 19:15:44 +0200

    stabilize angle at output of gru

--- model_gru.py
+++ model_gru.py
@@ -8,20 +8,20 @@ from utils import CONFIG
 class ModelGRU(nn.Module):
     def __init__(self):
         super().__init__()
-        self.num_layers = 3
+        self.hidden_size = CONFIG["training"]["hidden_size"],
+        self.num_layers = CONFIG["training"]["layers_count"],
 
         # input size is 2 (real and imaginary part)
         self.gru = nn.GRU(
             input_size=2,
-            hidden_size=CONFIG["training"]["hidden_size"],
-            num_layers=CONFIG["training"]["layers_count"],
+            hidden_size=self.hidden_size,
+            num_layers=self.num_layers,
             batch_first=True,
             dropout=0.1,
         )
 
         # map state into polynomial_degree * 2 (real and imaginary part for each zeroe)
-        self.output_projection = nn.Linear(
-            CONFIG["training"]["hidden_size"], CONFIG["polynomial_degree"] * 2
+        self.output_projection = nn.Linear(self.hidden_size, CONFIG["polynomial_degree"] * 2
         )
 
     def forward(self, x):
@@ -34,5 +34,7 @@ class ModelGRU(nn.Module):
         # map to output format polynomial_degree * 2 (real and imaginary part for each zeroe)
         out = self.output_projection(last_step)  # (B, 10)
         out = out.unflatten(-1, (CONFIG["polynomial_degree"], 2))  # (B, 5, 2)
+        log_mag = out[..., 0]
+        angle = torch.tanh(out[..., 1]) * np.pi
 
-        return out
+        return torch.stack([log_mag, angle], dim=-1)
