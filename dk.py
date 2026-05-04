2c6de9ed8ea2428c9844a9493b31e0a3c96a99a6
Arek <arek@arek.com>
2026-05-05 00:48:18 +0200

    use model for finding starting points

--- dk.py
+++ dk.py
@@ -2,17 +2,31 @@ import numpy as np
 from utils import CONFIG, dt
 import logging
 from joblib import Parallel, delayed
+import os
+from model_gru import ModelGRU
+import utils
+import torch
+import argparse
 
-def _calculate(coeffs, roots, max_iter, tolerance):
+def _calculate(coeffs, model, max_iter, tolerance):
     deg = CONFIG["polynomial_degree"]
     num_polys = len(coeffs)
-    
-    if not roots:
+
+    if model:
+        coeffs_torch = torch.view_as_real(torch.from_numpy(coeffs))
+        logging.info(f"Loading model: {model}")
+        with torch.no_grad():
+            r1 = torch.view_as_complex(model(coeffs_torch[0][None,:,:])).numpy()
+            roots = np.tile(r1, (num_polys, 1))
+
+    else:
         r = CONFIG["evaluation"]["dk_circle_radius"]
+        logging.info(f"No model passed, generate starting point on unit circle with r = {r}")
         roots_indices = np.arange(deg)
-        roots = r * np.exp(1j * roots_indices * 2 * np.pi / deg)
-        roots = np.tile(roots, (len(coeffs), 1))
-        
+        phi = (roots_indices * 2 * np.pi / deg) # 0.4 to break symmetry
+        roots = r * np.exp(1j * phi)
+        roots = np.tile(roots, (num_polys, 1))
+
     # reverse coefficients for np.polyval and ensure they are 2D
     c_rev = coeffs[:, ::-1] 
     r = roots.copy()
@@ -45,10 +59,12 @@ def _calculate(coeffs, roots, max_iter, tolerance):
         
         # Re-evaluate
         new_f0 = np.array([np.polyval(active_c[i], new_r[i]) for i in range(len(active_c))])
-        
         # Calculate convergence: 2-norm across the degree dimension
         # delta shape: (active_batch,)
-        delta = np.sqrt(np.sum((new_f0 - active_f0)**2, axis=1) / deg)
+        # delta = np.sqrt(np.sum(np.abs(new_f0 - active_f0)**2, axis=1) / deg)
+        delta = np.mean(np.abs(new_f0 - active_f0), axis=1)
+        # delta = np.linalg.norm((new_f0 - active_f0), ord=2, axis=1) / np.sqrt(deg)
+        # delta = np.linalg.norm(np.max(new_f0 - active_f0, 0.00001), ord=2, axis=1) / np.sqrt(deg)
         
         # Update active status
         converged_in_step = (delta <= tolerance)
@@ -70,7 +86,8 @@ def _calculate(coeffs, roots, max_iter, tolerance):
         "iterations_avg": (iterations_completed_total / (num_polys - polynomials_out_of_iterations)).item()
     }
 
-def durand_kerner(coeffs, roots: Optional = None, max_iter = CONFIG["evaluation"]["dk_max_iterations"], tolerance = CONFIG["evaluation"]["dk_tolerance"], n_cores = 1):
+def run(coeffs, model: Optional = None, max_iter = CONFIG["evaluation"]["dk_max_iterations"], tolerance = CONFIG["evaluation"]["dk_tolerance"], n_cores = 1):
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
@@ -81,21 +98,17 @@ def durand_kerner(coeffs, roots: Optional = None, max_iter = CONFIG["evaluation"
     )
     logging.info("=" * 50)
     logging.info(" Evaluate durand kerner ".center(50, "="))
-    logging.info(f" roots missing: {not roots}".center(50, "="))
+    logging.info(f" model is not provided: {not model}".center(50, "="))
     logging.info(f" {dt(1)} ".center(50, "="))
     logging.info("-" * 50)
     logging.info(f"defaults: {CONFIG} ".center(50, "="))
     logging.info("=" * 50)
 
     coeffs_splitted = np.array_split(coeffs, n_cores)
-    if roots:
-        roots_splitted = np.array_split(roots, n_cores)
-    else:
-        roots_splitted = [None]*n_cores
         
     logging.info("start")
     stats = Parallel(n_jobs=n_cores)(
-        delayed(lambda row: _calculate(row[0], row[1], max_iter, tolerance))(row) for row in zip(coeffs_splitted, roots_splitted)
+        delayed(lambda c: _calculate(c, model, max_iter, tolerance))(c) for c in coeffs_splitted
     )
     
     summary = {
@@ -106,7 +119,7 @@ def durand_kerner(coeffs, roots: Optional = None, max_iter = CONFIG["evaluation"
     }
     
     # Calculate the global average
-    # We use a conditional to avoid DivisionByZero if no correct roots exist
+    # We usea conditional to avoid DivisionByZero if no correct roots exist
     if summary["polynomials_correct_roots"] > 0:
         summary["iterations_avg"] = (
             summary["iterations_completed_total"] / summary["polynomials_correct_roots"]
@@ -117,3 +130,52 @@ def durand_kerner(coeffs, roots: Optional = None, max_iter = CONFIG["evaluation"
 
     logging.info("finished")
 
+def main():
+    parser = argparse.ArgumentParser(description="Run Durand-Kerner algorithm for evaluation")
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        help="Path to model",
+    )
+    parser.add_argument(
+        "-d",
+        "--data",
+        type=str,
+        default=CONFIG["evaluation"]["test_data_path"],
+        help="Path to test data",
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[logging.StreamHandler()],
+    )
+
+    logging.info("=" * 50)
+    logging.info(" Run Durand-Kerner algorithm for evaluation ".center(50, "="))
+    logging.info(f" {dt(1)} ".center(50, "="))
+    logging.info(f" {args} ".center(50, "="))
+    logging.info("-" * 50)
+    logging.info(f" {CONFIG} ".center(50, "="))
+    logging.info("=" * 50)
+
+    coeffs_path = os.path.join(args.data, "coefficients.npy")
+    logging.info(f"Load coefficients from {coeffs_path}")
+    coeffs_np_complex = np.load(coeffs_path)
+
+
+    if args.model:
+        logging.info(f"Load model from {args.model}")
+        model = ModelGRU()
+        model.load_state_dict(utils.read_compiled_model(args.model))
+        model.eval()
+    else:
+        logging.info(f"No model passed in args")
+        model = None
+
+    run(coeffs_np_complex[0:100], model)
+
+if __name__ == "__main__":
+    main()
