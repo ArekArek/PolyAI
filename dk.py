307f6ce6d0fbcd6dbe043123fd21464382b57813
Arek <arek@arek.com>
2026-05-06 20:21:32 +0200

    small refactor dk

--- dk.py
+++ dk.py
@@ -13,28 +13,29 @@ def _calculate(coeffs, model, max_iter, tolerance):
     deg = CONFIG["polynomial_degree"]
     num_polys = len(coeffs)
 
-
     if model:
         coeffs_torch = torch.view_as_real(torch.from_numpy(coeffs))
         logging.info(f"Loading model: {model}")
         with torch.no_grad():
-            roots = utils.polar_to_complex(model(coeffs_torch)).numpy()
+            starting_points = utils.polar_to_complex(model(coeffs_torch)).numpy()
 
     else:
         r = CONFIG["evaluation"]["dk_circle_radius"]
         logging.info(f"No model passed, generate starting point on unit circle with r = {r}")
         roots_indices = np.arange(deg)
-        phi = (roots_indices * 2 * np.pi / deg) # 0.4 to break symmetry
+        phi = (roots_indices * 2 * np.pi / deg) + 0.4 # 0.4 to break symmetry
         roots = r * np.exp(1j * phi)
-        roots = np.tile(roots, (num_polys, 1))
+        starting_points = np.tile(roots, (num_polys, 1))
 
 
-    # reverse coefficients for np.polyval and ensure they are 2D
+    # reverse coefficients for np.polyval
     c_rev = coeffs[:, ::-1].astype(dtype=complex) 
-    r = roots.copy().astype(dtype=complex)
+    r = starting_points.copy().astype(dtype=complex)
     
     f0 = np.array([np.polyval(c_rev[i], r[i]) for i in range(num_polys)])
-    mask = np.ones(num_polys, dtype=bool) # True = still iterating
+
+    # mask to select only polynomials which havent finished
+    mask = np.ones(num_polys, dtype=bool) # true/1 means still iterating
     iterations = np.zeros(num_polys, dtype=int)
     
     # calculate identity for the denominator trick
@@ -48,46 +49,27 @@ def _calculate(coeffs, model, max_iter, tolerance):
         if not np.any(mask):
             break
             
-        # Only update polynomials that haven't converged
+        # only update polynomials that haven't converged
         active_r = r[mask]
         active_f0 = f0[mask]
         active_c = c_rev[mask]
         
-        # Vectorized Aberth step for the active batch
-        # Shape of diffs: (active_batch, deg, deg)
+        # (zi - zj), eye is added so for i=j we have (zi-zi)+1=1 what will not break product
         diffs = active_r[:, :, None] - active_r[:, None, :] + eye
+        # product of (zi - zj)
         dens = np.prod(diffs, axis=2)
 
-        # Find where the denominator is dangerously small
-        #epsilon = 1e-12
-        #small_dens_mask = np.abs(dens) < epsilon
-
-        #if np.any(small_dens_mask):
-        #    # Generate tiny complex noise
-        #    noise = (np.random.randn(*dens.shape) + 1j * np.random.randn(*dens.shape)) * 1e-6
-
-        #    # Apply noise ONLY to the roots that are colliding
-        #    # active_r shape is (batch, deg), noise needs to match
-        #    active_r[small_dens_mask] += noise[small_dens_mask]
-
-        #    # Re-calculate diffs and dens after perturbation
-        #    diffs = active_r[:, :, None] - active_r[:, None, :] + eye
-        #    dens = np.prod(diffs, axis=2)
-        
-        # Update roots
         new_r = active_r - (active_f0 / dens)
 
         r[mask] = new_r
         
-        # Re-evaluate
         new_f0 = np.array([np.polyval(active_c[i], new_r[i]) for i in range(len(active_c))])
 
+        # check which polynomials are done
         delta = np.max(np.abs(new_r - active_r), axis=1)
-        
-        # Update active status
         converged_in_step = (delta <= tolerance)
         
-        # Logic to update global mask and iteration counts
+        # update global mask (for finished) and iteration counts (for active)
         active_indices = np.where(mask)[0]
         iterations[active_indices[~converged_in_step]] += 1
         mask[active_indices[converged_in_step]] = False
@@ -95,27 +77,6 @@ def _calculate(coeffs, model, max_iter, tolerance):
         f0[mask] = new_f0[~converged_in_step] # Update f0 for the next round
 
     return iterations
-
-def run(coeffs, model: Optional = None, max_iter = CONFIG["evaluation"]["dk_max_iterations"], tolerance = CONFIG["evaluation"]["dk_tolerance"], n_cores = 1):
-
-    coeffs_splitted = np.array_split(coeffs, n_cores)
-        
-    logging.info("start")
-    start_time = time.perf_counter()
-    iterations = Parallel(n_jobs=n_cores)(
-        delayed(lambda c: _calculate(c, model, max_iter, tolerance))(c) for c in coeffs_splitted
-    )
-    end_time = time.perf_counter()
-    logging.info("finished")
-    logging.info(f"run took {end_time - start_time:.4f} seconds")
-    iterations = np.concatenate(iterations)
-
-    logging.info(f"polynomials_total: {len(iterations)}")
-    logging.info(f"polynomials_out_of_iterations: {len(iterations[iterations >= max_iter])}")
-    logging.info(f"iterations_completed_total: {np.sum(iterations[iterations < max_iter])}")
-    logging.info(f"iterations_average: {np.average(iterations[iterations < max_iter])}")
-    logging.info(f"iterations_median: {np.median(iterations[iterations < max_iter])}")
-    
 
 def main():
     parser = argparse.ArgumentParser(description="Run Durand-Kerner algorithm for evaluation")
@@ -158,7 +119,7 @@ def main():
 
     coeffs_path = os.path.join(args.data, "coefficients.npy")
     logging.info(f"Load coefficients from {coeffs_path}")
-    coeffs_np_complex = np.load(coeffs_path)
+    coeffs = np.load(coeffs_path)
 
 
     if args.model:
@@ -170,7 +131,25 @@ def main():
         logging.info(f"No model passed in args")
         model = None
 
-    run(coeffs_np_complex, model, n_cores = args.cores)
+    coeffs_splitted = np.array_split(coeffs, args.cores)
+        
+    logging.info("start")
+    start_time = time.perf_counter()
+    
+    iterations = Parallel(n_jobs=args.cores)(
+        delayed(lambda c: _calculate(c, model, max_iter, tolerance))(c) for c in coeffs_splitted
+    )
+
+    end_time = time.perf_counter()
+    logging.info("finished")
+    logging.info(f"run took {end_time - start_time:.4f} seconds")
+    iterations = np.concatenate(iterations)
+
+    logging.info(f"polynomials_total: {len(iterations)}")
+    logging.info(f"polynomials_out_of_iterations: {len(iterations[iterations >= max_iter])}")
+    logging.info(f"iterations_completed_total: {np.sum(iterations[iterations < max_iter])}")
+    logging.info(f"iterations_average: {np.average(iterations[iterations < max_iter])}")
+    logging.info(f"iterations_median: {np.median(iterations[iterations < max_iter])}")
 
 if __name__ == "__main__":
     main()
